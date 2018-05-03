a8c34183115d7e8cab719240933d67a22429fdb8
Your Name <fjhbetter@163.com>
2018-05-03 13:34:53 +0800

    go 对mysql的增删改查

--- mysql.go
+++ mysql.go
@@ -6,11 +6,12 @@ import (
     _ "github.com/go-sql-driver/mysql"
 )
 
+
 var (
-    dbhostip = "127.0.0.1:3306"
-    dbusername = "root"
-    dbpassword = "123456"
-    dbname = "test"
+    dbhostip = "ip:port"
+    dbusername = "user"
+    dbpassword = "passwd"
+    dbname = "database"
 )
 
 func checkErr(err error) {
@@ -19,56 +20,89 @@ func checkErr(err error) {
     }
 }
 
-func insert() {
-    db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/Test?charset=utf8")
+func insertDb(recordDic map[string]string) bool {
+    db, err := sql.Open("mysql", "user:passwd@tcp(ip:port)/database?charset=utf8")
     checkErr(err)
-    stmt, err := db.Prepare("INSERT userinfo SET username=?,department=?,created=?")
+    stmt, err := db.Prepare("INSERT tbl_role SET name=?,create_time=?")
     checkErr(err)
-    res, err := stmt.Exec("fjh", "research", "2015")
+    res, err := stmt.Exec(recordDic["name"], recordDic["create_time"])
     checkErr(err)
     id, err := res.LastInsertId()
     checkErr(err)
     fmt.Println(id)
     db.Close()
+    return true
 }
 
-func update() {
-    db, err := sql.Open("mysql", "root:@/test?charset=utf-8")
+func updateDb(id int, name string) bool {
+    db, err := sql.Open("mysql", "user:passwd@tcp(ip:port)/database?charset=utf8")
     checkErr(err)
-    stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+    stmt, err := db.Prepare("update tbl_role set name=? where id=?")
     checkErr(err)
-    res, err = stmt.Exec("astaxieupdate", id)
+    res, err := stmt.Exec(name, id)
     checkErr(err)
     affect, err := res.RowsAffected()
     fmt.Println(affect)
     db.Close()
+    return true
 }
 
-func select() {
-    db, err := sql.Open("mysql", "root:@/test?charset=utf-8")
+func selectDb() map[int]map[string]string{
+    results := make(map[int]map[string]string)
+    itemDic := make(map[string]string)
+    db, err := sql.Open("mysql", "user:passwd@tcp(ip:port)/database?charset=utf8")
     checkErr(err)
-    rows, err := db.Query("SELECT * FROM userinfo")
+    rows, err := db.Query("SELECT id, name FROM tbl_role")
     checkErr(err)
+    i := 0
     for rows.Next() {
-        var uid int
-        var username string
-        err = rows.Scan(&uid, &username)
-        check(err)
-        fmt.Println(uid)
-        fmt.Println(username)
+        var id int
+        var name string
+        err = rows.Scan(&id, &name)
+        checkErr(err)
+        itemDic["id"] = string(id)
+        itemDic["name"] = name 
+        results[i] = itemDic
+        i = i + 1
     }
     db.Close()
+    return results 
 }
 
-
-func delete(){
-    db, err := sql.Open("mysql", "root:@/test?charset=utf-8")
+func deleteDb(id int) bool {
+    db, err := sql.Open("mysql", "user:passwd@tcp(ip:port)/database?charset=utf8")
     checkErr(err)
-    stmt, err = db.Prepare("delete from userinfo where uid=?")
+    stmt, err := db.Prepare("delete from tbl_role where id=?")
     checkErr(err)
-    res, err = stmt.Exec(id) 
-    affect, err = res.RowsAffected()
+    res, err := stmt.Exec(id) 
+    checkErr(err)
+    affect, err := res.RowsAffected()
     checkErr(err)
     fmt.Println(affect)
     db.Close()
+    return true
+}
+
+func main() {
+    //insert
+    insertDic := make(map[string]string)
+    insertDic["name"] = "role3"
+    insertDic["create_time"] = "1399064577"
+    ret := insertDb(insertDic)
+    if (! ret) {
+        fmt.Println("insert error")
+    }
+    //update
+    ret := updateDb(27, "role8")
+    if (! ret) {
+        fmt.Println("update error")
+    }
+    //select
+    results := selectDb()
+    fmt.Println(results)
+    //delete
+    ret := deleteDb(25)
+    if (! ret) {
+        fmt.Println("delete error")
+    }
 }
