f8dcfcaabeee206a412e044290e8e9f3bfe12314
Your Name <fjhbetter@163.com>
2018-05-07 13:07:23 +0800

    说明

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # goFunc
 ### 读写文件
   - file.go
-### mysql增删改查
+### mysql操作
   - 下载驱动: sudo go get github.com/go-sql-driver/mysql
   - mysql.go
 ### 时间转换
   - time.go
+### json组装和解析
+  - json.go
 ### 网页抓取
+  - downPage.go 
 ### 并行调度
 ### socket操作
--- downPage.go
+++ downPage.go
@@ -12,7 +12,7 @@ import (
 /*
 简单的网页抓取，相当于wget
 */
-func getPage(){
+func httpGet(){
     resp, err := http.Get("http://www.baidu.com")
     if err != nil {
         fmt.Println(err)
@@ -23,6 +23,32 @@ func getPage(){
         fmt.Println(err)
     }
     fmt.Println(string(b))
+}
+
+/*
+网页抓取，post请求
+*/
+func httpDo() {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", "http://www.01happy.com/demo/accept.php", strings.NewReader("name=cjb"))
+	if err != nil {
+		// handle error
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", "name=anny")
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(string(body))
 }
 
 func Socks5Client(add string, auth ...*proxy.Auth) (client *http.Client, err error){
